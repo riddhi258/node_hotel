d3935324d0733b48523f66a041f1ed6c456f005f
Riddhi2909 <upadhyayriddhi445@gmail.com>
2024-10-11 16:33:22 +0530

    changes done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { size } = require('lodash');
 const app = express();
 const db = require ('./db');
-const Person =require ('./models/person');
+const Person= require ('./models/person');
 const menuItem = require('./models/menuItem');
 const passport = require ('./auth');
 
@@ -15,7 +15,7 @@ app.use(bodyParser.json())//req body
 app.use(passport.initialize());
 const LocalAuthMiddleWare= passport.authenticate('local',{session:false})
 
-app.get('/', LocalAuthMiddleWare, function(req,res)  {
+app.get('/', function(req,res)  {
   res.send('wlecom to our Hotel ')
 })
 /*app.get('/chai', function (req, res) {
@@ -37,7 +37,7 @@ app.get('/', LocalAuthMiddleWare, function(req,res)  {
 // import the router files
 const personRoute= require ('./routes/personRoute');
 // use the router files
-app.use('/person',personRoute);
+app.use('/person',LocalAuthMiddleWare,personRoute);
 
 const menuItemRoutes= require ('./routes/menuItemRoutes');
 // use the router files
