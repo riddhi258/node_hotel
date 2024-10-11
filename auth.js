0f9c457c180267b5205a6d66136717323c431545
Riddhi2909 <upadhyayriddhi445@gmail.com>
2024-10-11 16:41:26 +0530

    commit

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const Person =require ('./models/person');
 passport.use(new LocalStrategy(async (username, password, done) => {
     try {
-      console.log('received credentials:', username, password);
+      //console.log('received credentials:', username, password);
       const user = await Person.findOne({ username });  // Use await to fetch user
   
       if (!user)
